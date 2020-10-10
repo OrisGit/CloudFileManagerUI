e635f797a464ac58d72794cb5aab7f7d54ef7eca
oris <chief.oris@gmai.com>
2020-10-10 14:19:25 +0400

    Basic context menu implementation

--- src/app/components/file-manager/file-manager.component.ts
+++ src/app/components/file-manager/file-manager.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {SessionService} from '../../service/session.service';
 import {Directory} from '../../model/directory';
@@ -11,13 +11,17 @@ import {ContentManagerService} from '../../service/content-manager.service';
 })
 export class FileManagerComponent implements OnInit {
 
+  @ViewChild('contextMenu', {static: false})
+  contextMenu: ElementRef;
+
   fileUploaderIsOpen = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private session: SessionService,
-    public contentManager: ContentManagerService
+    public contentManager: ContentManagerService,
+    private renderer: Renderer2
   ) {
     console.log('Costructor');
     if (!this.session.isLoggedIn) {
@@ -43,5 +47,21 @@ export class FileManagerComponent implements OnInit {
 
   openDirectoryByIndex(index: number): void {
     this.contentManager.loadContentForDirectoryByIndex(index);
+  }
+
+  onRightClick(event): boolean {
+    const top = event.pageY;
+    console.log('top ' + event.pageY);
+    const left = event.pageX;
+    console.log('left ' + event.pageX);
+    this.contextMenu.nativeElement.setAttribute('style', 'display: block; top: ' + top + 'px; left: ' + left + 'px;');
+    this.renderer.addClass(this.contextMenu.nativeElement, 'show');
+
+    return false;
+  }
+
+  closeContextMenu() {
+    this.renderer.removeClass(this.contextMenu.nativeElement, 'show');
+    this.contextMenu.nativeElement.removeAttribute('style');
   }
 }
